2f65ed476efdc2882b6f79b0ad82d12793c98843
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-10-22 14:38:57 +0200

    Completo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-var emailArray = ["pippo@gmail.com", "pluto@gmail.com","paperino@gmail.com","topolino@gmail.com"];
+//Esercizio Email
+
+var emailArray = ["pippo@gmail.com", "pluto@gmail.com","paperino@gmail.com","topolino@gmail.com"]; // Array Email
 var userEmail = prompt("Acesso limitato !! Inserisci la tua email");
 var message = "Email non presente"
 
@@ -8,23 +10,28 @@ for (var i = 0 ; i < emailArray.length ; i ++){
      }
 }
 console.log(message);
+document.getElementById("answer").innerHTML = message;
 
 
 
 
 
+// Esercizio Dadi
 
-var dadoUser = [1,2,3,4,5,6];
-var dadoPc = [1,2,3,4,5,6];
+var dadoUser = [1,2,3,4,5,6]; //Array Dado User
+var dadoPc = [1,2,3,4,5,6]; //Array Dado PC
 var playEl = document.getElementById("play");
 
+//Premere il bottone nella pagina per giocare!
 playEl.addEventListener("click",
 function(){
     var i = Math.floor(Math.random() * (5 - 0)) + 0;
     var v = Math.floor(Math.random() * (5 - 0)) + 0;
 if ( dadoUser[i] > dadoPc[v] ){
     console.log("Hai Vinto");
+    document.getElementById("risultato").innerHTML = "Hai Vinto"
     } else {
     console.log("Hai Perso");
+    document.getElementById("risultato").innerHTML = "Hai Perso"
     }
 })
